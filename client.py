bb0aca09fddb5e65fded7a3fe5828e3be3a69242
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-03 15:21:11 +0530

    added more functionality
    We can now send python objects as well

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 
 HEADERSIZE = 10
@@ -8,7 +9,7 @@ s.connect((socket.gethostname(), 1234))
 
 while True:
 
-    full_message = ''
+    full_message = b''
     new_message = True
     while True:
         message = s.recv(16)
@@ -17,11 +18,16 @@ while True:
             message_length = int(message[:HEADERSIZE])
             new_message = False
 
-        full_message += message.decode("utf-8")
+        # full_message += message.decode("utf-8")
+        full_message += message
 
         if len(full_message) - HEADERSIZE == message_length:
             print("Full message received!")
             print(full_message[HEADERSIZE:])
+
+            d = pickle.loads(full_message[HEADERSIZE:])
+            print(d)
+
             new_message = True
             full_message = ''
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import pickle
 
 
 HEADERSIZE = 10
@@ -14,13 +15,17 @@ while True:
     client_socket, address = s.accept()
     print(f"Connection from {address} has been established!")
 
-    msg = 'Welcome to the sever!'
-    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    # msg = 'Welcome to the sever!'
 
-    client_socket.send(bytes(msg, "utf-8"))
+    d = {1: "Hey", 2: "There"}
+    msg = pickle.dumps(d)
 
-    while True:
-        time.sleep(3)
-        msg = f"The time is: {time.time()}"
-        msg = f'{len(msg):<{HEADERSIZE}}' + msg
-        client_socket.send(bytes(msg, "utf-8"))
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
+
+    client_socket.send(msg)
+
+    # while True:
+    #     time.sleep(3)
+    #     msg = f"The time is: {time.time()}"
+    #     msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    #     client_socket.send(bytes(msg, "utf-8"))
