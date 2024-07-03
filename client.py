3bae38c0ddbbce8c369673ea2b7e7e4c9fa2ad7c
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-03 14:59:04 +0530

    updated files
    The data stream is continuous now and doesn't terminate after running just once and to test this, it is sending time every three seconds.

--- client.py
+++ client.py
@@ -1,9 +1,28 @@
 import socket
 
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 while True:
-    message = s.recv(1024)
-    print(message.decode("utf-8"))
+
+    full_message = ''
+    new_message = True
+    while True:
+        message = s.recv(16)
+        if new_message:
+            print(f"New message length: {message[:HEADERSIZE]}")
+            message_length = int(message[:HEADERSIZE])
+            new_message = False
+
+        full_message += message.decode("utf-8")
+
+        if len(full_message) - HEADERSIZE == message_length:
+            print("Full message received!")
+            print(full_message[HEADERSIZE:])
+            new_message = True
+            full_message = ''
+
+    print(full_message)
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
+import time
 
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # AF_INET corresponds to IPv4, SOCK_STREAM corresponds to TCP
@@ -10,5 +13,14 @@ s.listen(5)
 while True:
     client_socket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    client_socket.send(bytes("Welcome to my server!", "utf-8"))
-    client_socket.close()
+
+    msg = 'Welcome to the sever!'
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    client_socket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        client_socket.send(bytes(msg, "utf-8"))
